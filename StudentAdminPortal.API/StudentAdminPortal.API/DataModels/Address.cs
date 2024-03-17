3fac3a8681516bac4a6a8c652bc6acce4d905848
Edwin <edwinrhc@gmail.com>
2024-03-17 01:26:04 -0500

    DataModel Addres, Gender, Student, StudentAdminContext

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        // Navigation property
+        public Guid StudentId { get; set; }
+
+
+
+    }
+}
